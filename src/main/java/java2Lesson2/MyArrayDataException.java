f74125c90efe34812483f0430a655357df156e6b
Samelena <lyly-miranda@yandex.ru>
2021-05-15 20:30:22 +0300

    Initial commit

---
+++ src/main/java/java2Lesson2/MyArrayDataException.java
@@ -0,0 +1,11 @@
+package java2Lesson2;
+
+public class MyArrayDataException extends RuntimeException {
+    int i, j;
+
+    public MyArrayDataException(int i, int j) {
+        super("Wrong data. You have: row " + i + ", column " + j);
+        this.i = i;
+        this.j = j;
+    }
+}
---
+++ src/main/java/java2Lesson2/MyArraySizeException.java
@@ -0,0 +1,15 @@
+package java2Lesson2;
+
+public class MyArraySizeException extends RuntimeException{
+    int row, column;
+
+    public MyArraySizeException(int row, int column) {
+        super("Wrong data, row and column can be only 4. You have: row " + row + ", column " + column);
+        this.row = row;
+        this.column = column;
+    }
+
+    public MyArraySizeException(String format) {
+
+    }
+}
